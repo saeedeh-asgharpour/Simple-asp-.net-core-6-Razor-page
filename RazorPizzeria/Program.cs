5063130ff419683c88815ceb94035c49798ce8d8
saeedeh-asgharpour <saeedeh.asqarpour@gmail.com>
2022-08-11 18:59:00 +0430

    Add a SQL Database using Entity Framework Core

--- RazorPizzeria/Program.cs
+++ RazorPizzeria/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using RazorPizzeria.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
